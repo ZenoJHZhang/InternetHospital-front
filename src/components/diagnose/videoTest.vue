cb07036452e694dfdf4eaef6821acd999aa37ee2
张江浩 <zeno.jh.zhang@gmail.com>
2019-03-28 13:51:48 +0800

    no message

--- src/components/diagnose/videoTest.vue
+++ src/components/diagnose/videoTest.vue
@@ -18,18 +18,23 @@ import axion from "@/utils/http_url";
 export default {
   data() {
     return {
-      predefinedRoomId: '',
+      predefinedRoomId: "",
       connection: ""
     };
   },
   methods: {
     openRoom() {
-      this.connection.open(this.predefinedRoomId);
+      this.connection.open(this.predefinedRoomId),
+        function(isRoomOpened, roomid, error) {
+          if (isRoomOpened === true) {
+            this.joinRoom();
+          }
+        };
     },
     joinRoom() {
       this.connection.join(this.predefinedRoomId);
     },
-    connectVideo(){
+    connectVideo() {
       this.connection = new RTCMultiConnection();
 
       // this line is VERY_important
@@ -49,7 +54,7 @@ export default {
       };
 
       this.connection.onstream = function(event) {
-        console.log(event)
+        console.log(event);
         if (event.type == "local") {
           document.getElementById("localVideo").srcObject = event.stream;
         } else if (event.type == "remote") {
@@ -60,51 +65,52 @@ export default {
   },
   mounted() {
     this.$nextTick(function generate() {
-        axion.userAuthorizationTest();
-        this.connectVideo();
-        this.predefinedRoomId = 'zjh';
-        this.openRoom();
+      axion.userAuthorizationTest();
+      this.connectVideo();
+      this.predefinedRoomId = "zjh";
+      this.openRoom();
     });
   },
-  destroyed(){
+  destroyed() {
     this.connection.closeSocket();
   }
 };
 </script>
 <style scoped>
-    .container{
-        position: relative;
-        width:  80%;
-        padding: 0;
-        height: 600px;
-        padding-left:10%
-    }
-    .bigVideo{
-        width: 100%;
-        height: 100%;
-        border:1px solid #ccc;
-    }
-    .smallVideo{
-        position: absolute;
-        display: inline-block;
-        right: 0;
-        bottom: 0;
-        width: 200px;
-        height: 150px;
-        cursor: pointer;
-    }
-    .smallVideo:hover{
-        background-color: rgba(255,255,255,0.2);
-        opacity: 0.2;
-    }
-    .video{
-        position:absolute;
-        width: 100%;
-        height: 100%;
-    }
-    .bigVideo p
-    {margin-top:25%;
-        text-align: center;color: #e2e2e2;
-        vertical-align: middle;
-    }
+.container {
+  position: relative;
+  width: 80%;
+  padding: 0;
+  height: 600px;
+  padding-left: 10%;
+}
+.bigVideo {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #ccc;
+}
+.smallVideo {
+  position: absolute;
+  display: inline-block;
+  right: 0;
+  bottom: 0;
+  width: 200px;
+  height: 150px;
+  cursor: pointer;
+}
+.smallVideo:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+  opacity: 0.2;
+}
+.video {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.bigVideo p {
+  margin-top: 25%;
+  text-align: center;
+  color: #e2e2e2;
+  vertical-align: middle;
+}
 </style>
